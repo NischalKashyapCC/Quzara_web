f550e985034264fa9ba1fe3479251cf724c2f467
lokrajb26 <lokrajb26@gmail.com>
2018-11-28 18:51:54 +0530

    added the particles

--- public/custom.js
+++ public/custom.js
@@ -128,3 +128,8 @@ $(document).ready(function () {
         }]
     });
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
